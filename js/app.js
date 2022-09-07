4d7452336101cb016e51e8f31a69ac2af0286e5c
Lucas <lucasjs.developer@gmail.com>
2022-09-07 20:21:41 -0300

    Added modal

--- js/app.js
+++ js/app.js
@@ -23,3 +23,36 @@ function closeMsg() {
 }
 
 $(document).on('click', '#msg .close', closeMsg);
+
+function loading(status, e, ...h) {
+
+    h.forEach(element => {
+        if (status) {
+            element.hide();
+        } else {
+            element.show();
+        }
+    });
+
+    if (typeof e === 'object') {
+        if (status) {
+            e.show();
+        } else {
+            e.hide();
+        }
+    }
+}
+
+$(document).on('click', '.modal', function(e){
+
+    if (e.target === this) {
+        $(this).hide();
+    }
+
+});
+
+$(document).on('click', '.modal .btn_cancel', function(){
+
+    $(this).parents('.modal').hide();
+
+});
