45642b83fe03c5502ee792acc95e83354bd6a164
Lucas <lucasjs.developer@gmail.com>
2022-09-08 22:09:57 -0300

    Adjusted the response for method POST

--- App/Controller/App.php
+++ App/Controller/App.php
@@ -19,13 +19,30 @@ class App {
         define('PG_TITLE', $this->route->title);
         $data = $this->route->callMethod();
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data['response']['status'] = 200;
+
+            exit(json_encode($data['response']));
+        }
+
         View::getFile('App', $data);
     }
 
     private function validateAccess(): void
     {
-        if (!$this->route->valid_access)
-            header('Location: /login');
+        if ($this->route->valid_access)
+            return;
+
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'POST':
+                exit(json_encode([
+                    'error' => 'Acesso negado.',
+                    'status' => '403'
+                ]));
+            default:
+                header('Location: /login');
+                break;
+        }
     }
 
 }
